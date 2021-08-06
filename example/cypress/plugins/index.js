2487e39bba887f591d3151d4ed7738c0fe2200d4
Emanuel Suriano <emanuel.suriano@revolut.com>
2021-08-06 10:45:01 +0200

    add missing task command

--- example/cypress/plugins/index.js
+++ example/cypress/plugins/index.js
@@ -1,4 +1,18 @@
-module.exports = (_, config) => {
+module.exports = (on, config) => {
   config.env.PERCY_TOKEN = process.env.PERCY_TOKEN;
+
+  on('task', {
+    log(message) {
+      console.log(message);
+
+      return null;
+    },
+    table(message) {
+      console.table(message);
+
+      return null;
+    },
+  });
+
   return config;
 };
