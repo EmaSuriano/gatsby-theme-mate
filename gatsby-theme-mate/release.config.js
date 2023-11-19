013e87d858c1eba84f4f998cf5e8d6744aa6005f
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-11-19 23:14:08 +0100

    chore: add release config

---
+++ gatsby-theme-mate/release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
