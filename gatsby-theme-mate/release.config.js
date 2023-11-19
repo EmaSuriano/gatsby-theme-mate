df350ca7e8c1153dbdd9717737383e88c9ad7f7d
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-11-19 23:24:35 +0100

    chore: no changelog

--- gatsby-theme-mate/release.config.js
+++ gatsby-theme-mate/release.config.js
@@ -3,12 +3,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
     '@semantic-release/npm',
     '@semantic-release/github',
     [
