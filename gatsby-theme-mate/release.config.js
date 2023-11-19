976798f41092cd634aaf106a09af73f7e8e22a9f
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-11-19 23:22:16 +0100

    chore: add npm publish

--- gatsby-theme-mate/release.config.js
+++ gatsby-theme-mate/release.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-  branches: ['master'],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
       '@semantic-release/git',
       {
-        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+        assets: ['CHANGELOG.md', 'dist/**'],
         message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
